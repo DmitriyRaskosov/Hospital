1863621f1c2dc06ce4607bbbd4eef088f5cb3196
DmitriyRaskosov <DmitriyRaskosov@yandex.ru>
2023-09-29 14:56:33 +0300

    update_appointment endpoint

--- api/update_appointment.php
+++ api/update_appointment.php
@@ -1,7 +1,17 @@
 <?php
 
 $upd_id = $_GET['id'];
+$upd_date = $_GET['date'];
 
 $appointments = file_get_contents('../data/appointments.json');
 $appointments = (array)json_decode($appointments, true);
 
+foreach ($appointments as $key => $value) {
+	if ($upd_id == $value['id']) {
+		$appointments[$key]['appointment_date'] = $_GET['date'];
+		break;
+	}
+}
+
+$appointments = json_encode($appointments);
+file_put_contents('../data/appointments.json', $appointments);
