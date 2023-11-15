6156c2a1d5dc6ec3c3ee5eae6230dbae402d61f6
DmitriyRaskosov <DmitriyRaskosov@yandex.ru>
2023-11-15 17:59:24 +0300

    несущественные правки

--- api/api.php
+++ api/api.php
@@ -4,14 +4,20 @@ include_once ('classes/controller_app.php');
 
 $uri_in_array = explode('/', $_SERVER['REQUEST_URI']);
 
+// запрос пользователя в виде массива, из которого можно будет понять, что именно пользователь хочет сделать
 $request = [];
 $request['method'] = $_SERVER['REQUEST_METHOD'];
 $request['ctrl_request'] = $uri_in_array[3];
 if (isset($uri_in_array[4]) && $uri_in_array[4] != null) {
 	$request['id'] = $uri_in_array[4];
 }
-
 print_r($request);
+
+// данные, передаваемые пользователем; кода будет больше, но пока вот так - парсинг будет здесь
+$input_data = $_REQUEST;
+print_r($input_data);
+
+
 
 if ($request['ctrl_request'] == 'appointments') {
 	$result = ControllerAppoinment::getData($request);
--- api/classes/AppointmentController.php
+++ api/classes/AppointmentController.php
@@ -3,7 +3,7 @@
 require_once ('controller.php');
 require_once('model_appointment.php');
 
-class ControllerAppoinment extends Controller 
+class AppointmentController extends Controller 
 {
 	
 }
