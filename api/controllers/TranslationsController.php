78c7a773a86c55079446b2645ee524bf7210955b
DmitriyRaskosov <DmitriyRaskosov@yandex.ru>
2024-01-06 13:03:43 +0300

    api работает со всеми контроллерами и сущностями корректно, кроме конкретной выдачи результата работы пользователю

--- api/controllers/TranslationsController.php
+++ api/controllers/TranslationsController.php
@@ -1,1 +1,10 @@
 <?php
+require_once __DIR__.'/../controllers/AbstractController.php';
+require_once __DIR__.'/../models/Translations.php';
+
+
+class TranslationsController extends AbstractController {
+
+    public static $model_name = 'Translations';
+
+}
