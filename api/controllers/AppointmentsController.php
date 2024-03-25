4e79394128787f75322c9998430229b15cd3b4dc
DmitriyRaskosov <DmitriyRaskosov@yandex.ru>
2024-03-25 22:40:03 +0300

    потихоньку прикручиваю новую валидацию

--- api/controllers/AppointmentsController.php
+++ api/controllers/AppointmentsController.php
@@ -6,25 +6,4 @@ class AppointmentsController extends AbstractController {
 
     public static $model_name = 'Appointments';
 
-    public function validation ()
-    {
-        foreach ($array as $key => $value) {
-            if ($key == 'doctor_id') {
-                $data = Database::getConnect()->query('SELECT * FROM '.'Doctors'.' WHERE id='.$value);
-            }
-            if ($key == 'app_date') {
-                if (!is_int($value)) {
-                    throw new Exception ($key." неправильно указано");
-                }
-                $data = Database::getConnect()->query('SELECT * FROM '.static::$table_name.' WHERE doctor_id='.$value);
-            }
-            if ($data == false) {
-                throw new Exception ($key." не существует");
-            }
-        }
-
-
-        return true;
-    }
-
 }
--- api/controllers/PatientsController.php
+++ api/controllers/PatientsController.php
@@ -2,9 +2,26 @@
 require_once __DIR__.'/../controllers/AbstractController.php';
 require_once __DIR__.'/../models/Patients.php';
 
-
 class PatientsController extends AbstractController {
 
     public static $model_name = 'Patients';
 
+    public static function validate($valid_data)
+    {
+        foreach ($valid_data as $key => $value) {
+            if (strlen($value) < 1) {
+                throw new exception ("имя / фамилия не могут быть пустыми");
+            }
+            if (ctype_alpha($value) == false) {
+                throw new exception ("имя / фамилия не должны содержать цифры");
+            }
+        }
+        return true;
+    }
+
+    public function create($post)
+    {
+        self::validate($post);
+        return parent::create($post);
+    }
 }
